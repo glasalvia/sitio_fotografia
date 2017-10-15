2fdfccccec01b63303190b6dd633d56d78dee462
Gonzalo <glasalviacalio@gmail.com>
2017-10-15 11:48:10 -0300

    Se añadieron rutas para contacto y sobre mi. Se estabeclio / como home. Se importan los modelos correspondientes

--- sitio_fotografia/apps/portal/urls.py
+++ sitio_fotografia/apps/portal/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
-from apps.portal.views import index_portal
+from django.conf.urls import url, include
+from apps.portal.views import index_portal, index_contacto, index_sobre_mi, success
 
 
 urlpatterns = [
-	url(r'^$', index_portal),
+	url(r'^$', index_portal, name='index'),
+	url(r'^contacto/$', index_contacto, name='contacto'),
+	url(r'^sobre_mi/$', index_sobre_mi, name='sobre_mi'),
+	url(r'^success/$', success, name='success'),
 ]
+
