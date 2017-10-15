bee1ec75d6724c819d3436a834f97064ba75e5b1
Gonzalo <glasalviacalio@gmail.com>
2017-10-14 21:48:23 -0300

    Se registran aplicaciones equipo y albums. Tambien se agregan las variables para los paths de media

--- sitio_fotografia/sitio_fotografia/settings.py
+++ sitio_fotografia/sitio_fotografia/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.portal',
+    'apps.equipo',
+    'apps.albums',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
